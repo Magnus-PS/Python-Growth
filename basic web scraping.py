25b2790a6dcd66ae3621c0edaade32c9aa0e3ddc
Magnus-PS <60468157+Magnus-PS@users.noreply.github.com>
2021-01-16 10:38:57 -0500

    Add files via upload

--- basic web scraping.py
+++ basic web scraping.py
@@ -4,19 +4,20 @@ from csv import writer
 
 #Download data
 response = requests.get("https://www.rithmschool.com/blog")
-#print(response.text) #verify that we've downloaded HTML
 soup = BeautifulSoup(response.text, "html.parser")
 articles = soup.find_all("article")
 
+#Create csv file to be written to
 with open("blog_data.csv", "w") as csv_file:
     csv_writer = writer(csv_file) 
     csv_writer.writerow(["title", "link", "date"])
 
-#use developer tools to identify code block(s) of interest and then retrieve article titles
+#Use developer tools to identify code block(s) of interest and then retrieve article titles
     for article in articles:
         a_tag = article.find("a")
         title = a_tag.get_text() 
         url = a_tag["href"]
         date = article.find("time")["datetime"]
+        #For each blog entry (on the page), write to file
         csv_writer.writerow([title, url, date])
     
